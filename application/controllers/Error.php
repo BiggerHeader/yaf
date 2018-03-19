a8bb7f7e24249e5cc49069a6f8081b30f8f36e04
penghui <penghui@che001.com>
2018-03-19 19:39:19 +0800

    邮件发送 队列 已完成

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -5,7 +5,8 @@ class ErrorController extends Yaf_Controller_Abstract
 {
     // 异常捕获
     public function errorAction(Exception $exception)
-    {        
-        echo  'error';exit();
+    {
+        var_dump($exception);
+        exit();
     }
 }
